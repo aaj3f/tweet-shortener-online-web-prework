8d22fd3f385b4ac6ee7211264eaaf057161fe567
Andrew Johnson <ajohnson.uva@gmail.com>
2018-10-19 17:36:28 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -13,8 +13,8 @@ end
 
 def word_substituter(tweet)
   array = tweet.split.collect do |word|
-    if dictionary.keys.any? {|w| w.to_s == word}
-      word = dictionary[:word].values
+    if dictionary.keys.any? {|w| w == word}
+      word = dictionary[word].values
     else word
     end
   end
