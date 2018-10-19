79f40ff2a48e3387dc97832df0da6476535c5bf7
Andrew Johnson <ajohnson.uva@gmail.com>
2018-10-19 17:43:34 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -14,7 +14,7 @@ end
 
 def word_substituter(tweet)
   array = tweet.split.collect do |word|
-    if dictionary.keys.any? {|w| w == word}
+    if dictionary.keys.any? {|w| w == word.lowcase}
       word = dictionary[word]
     else word
     end
